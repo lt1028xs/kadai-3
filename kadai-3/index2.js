1e9c20a157c977311c120c510366523ef12304cd
lt1028xs <uk0157.ky@gmail.com>
2019-11-06 04:09:06 +0000

    htmlを追加

---
+++ kadai-3/index2.js
@@ -0,0 +1,14 @@
+for (let index = 0; index < 100;  index++) {
+    console.log(index)
+}
+
+//１から１００までの和を求める
+let sum = 0
+
+for (let n = 0; n <= 100; n - n +1){
+    sum = sum + n
+}
+console.log('1から　'　+ n + '　までの和は　' + sum + 'です')
+sum = sum + n
+}
+console.log(sum)
