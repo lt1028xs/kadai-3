8ad3777bb7a68e3f73d8001a63ce84fe5bdf5a5f
lt1028xs <uk0157.ky@gmail.com>
2019-11-20 04:50:26 +0000

    課題5を追加

---
+++ kadai-5/index.js
@@ -0,0 +1,25 @@
+const profile = {
+    firstName: 'Hanako',
+    familyName: 'Tanaka',
+    age: 20,
+    major: 'Art',
+    hobby: 'Listen to music',
+    job: 'teacher',
+    families: 'Dad Mam Dog'
+};
+
+const keys = Object.keys(profile);
+
+for (let i = 0; i < keys.length; i = i + 1) {
+    const key = keys[i];
+    const value = profile[key];
+    const text = '私の' + key + 'は、'　+ value + 'です。'
+    console.log(text);
+}
+
+for (let key of keys){
+    const value = profile[key];
+    const text = "私の" + key + "は、" + value + "です。";
+    console.log(text);
+}
+
