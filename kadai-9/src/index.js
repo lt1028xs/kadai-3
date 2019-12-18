b9d6f15a99135f0e40edb579bb7a651f3477f339
lt1028xs <uk0157.ky@gmail.com>
2019-12-18 05:29:15 +0000

    課題9を追加

--- kadai-9/src/index.js
+++ kadai-9/src/index.js
@@ -1,5 +1,7 @@
-const a = require('./module-a.js')
-const b = require('./module-b.js')
+const a = require('./module-a')
+const b = require('./module-b')
+const c = require('./module-c')
 
 a()
 b()
+console.log(c)
